825ea8589ded33f1f829d9673f081c7cea16e74f
Robbie McCorkell <robbiemccorkell@gmail.com>
2017-12-22 11:27:17 +0000

    add eslint

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Changes you make will automatically reload.</Text>
+    <Text>Shake your phone to open the developer menu.</Text>
+  </View>
+);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
