8012a9c2cdfb3e288a57d7500717f60e97e1415c
Robbie McCorkell <robbiemccorkell@gmail.com>
2018-01-14 22:33:18 +0000

    render list view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { createClient } from "contentful/dist/contentful.browser.min";
 import config from "./config";
 
@@ -40,11 +40,15 @@ export default class extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        {this.state.events.items.map(event => (
-          <View key={event.sys.id}>
-            <Text>{event.fields.name}</Text>
-          </View>
-        ))}
+        <FlatList
+          data={this.state.events.items}
+          keyExtractor={item => item.sys.id}
+          renderItem={({ item: event }) => (
+            <View key={event.sys.id}>
+              <Text>{event.fields.name}</Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
@@ -54,6 +58,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    paddingTop: 40,
     backgroundColor: "#fff",
     alignItems: "flex-start",
     justifyContent: "center"
