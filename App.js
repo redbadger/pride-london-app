268706b1ac9e1197d48793012e2ef7c5fd88df81
Robbie McCorkell <robbiemccorkell@gmail.com>
2018-01-20 17:40:43 +0000

    fix eslint

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
 // @flow
 
-import React, { Component } from "react";
+import React from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  ios: "Press Cmd+R to reload \n Cmd+D or shake for dev menu",
   android:
     "Double tap R on your keyboard to reload,\n" +
     "Shake or press menu button for dev menu"
 });
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Pride London!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to Pride London!</Text>
+    <Text style={styles.instructions}>To get started, edit App.js</Text>
+    <Text style={styles.instructions}>{instructions}</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -40,3 +36,5 @@ const styles = StyleSheet.create({
     marginBottom: 5
   }
 });
+
+export default App;
--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,8 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import "react-native";
+import renderer from "react-test-renderer";
+import React from "react";
+import App from "../App";
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
+it("renders correctly", () => {
+  expect(() => renderer.create(<App />)).not.toThrow();
 });
