1a126b9c6a352bf4b77ee3829e17e313976f6cfc
Robbie McCorkell <robbiemccorkell@gmail.com>
2018-01-20 19:34:14 +0000

    fix test step

--- App.test.js
+++ App.test.js
@@ -1,7 +1,7 @@
 import "react-native";
 import renderer from "react-test-renderer";
 import React from "react";
-import App from "../App";
+import App from "./App";
 
 it("renders correctly", () => {
   expect(() => renderer.create(<App />)).not.toThrow();
