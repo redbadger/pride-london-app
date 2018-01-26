535c99c1a44be60e03bb20329031be47b1162ba7
Mark Holland <mark.holland@red-badger.com>
2018-01-26 16:29:38 +0000

    Ensure content never enters status bar

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// @flow
 
-AppRegistry.registerComponent('PrideLondonApp', () => App);
+import React from "react";
+import { AppRegistry, SafeAreaView, StyleSheet } from "react-native";
+import App from "./App";
+
+const Main = () => (
+  <SafeAreaView style={styles.safeArea}>
+    <App />
+  </SafeAreaView>
+);
+
+const appColor = "#ddd"; // Color consistent with app
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: appColor
+  }
+});
+
+AppRegistry.registerComponent("PrideLondonApp", () => Main);
