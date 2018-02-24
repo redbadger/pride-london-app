e2074f9eb48a7d002f6628e8ad9671922b4eaca4
Robbie McCorkell <robbiemccorkell@gmail.com>
2018-02-24 19:11:51 +0000

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import React, { Component } from "react";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers";
 import { getEvents } from "./actions";
 import App from "./App";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 class AppWrapper extends Component<{}> {
   componentDidMount() {
