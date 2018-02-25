17c6137c6bf2c61d1ba319af893d9d030f3fbab9
Mark Holland <mark@partiallogic.com>
2018-02-25 15:23:24 +0000

    Experimenting with beta by Fabric

--- ios/PrideLondonApp/AppDelegate.m
+++ ios/PrideLondonApp/AppDelegate.m
@@ -12,10 +12,14 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [Fabric with:@[[Crashlytics class]]];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
