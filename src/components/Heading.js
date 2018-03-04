1b8a10a3ca62de3b5c0db227fab76bfa6ee19f38
Robbie McCorkell <robbiemccorkell@gmail.com>
2018-03-04 13:34:03 +0000

    add fonts

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -6,11 +6,16 @@ type Props = {
   text: string
 };
 
-const Heading = ({ text }: Props) => <Text style={styles.h1}>{text}</Text>;
+const Heading = ({ text }: Props) => (
+  <Text style={[styles.heading, styles.h1]}>{text}</Text>
+);
 
 export default Heading;
 
 const styles = StyleSheet.create({
+  heading: {
+    fontFamily: "Poppins-Bold"
+  },
   h1: {
     fontSize: 24,
     lineHeight: 28
