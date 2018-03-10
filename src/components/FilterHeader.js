4cb1d523c034e2a76034266c9b266a6a321b17c4
Jan Kuehle <jan.kuehle@red-badger.com>
2018-03-10 19:33:07 +0000

    Add draft of filters for Android and iOS to filter header

--- src/components/FilterHeader.js
+++ src/components/FilterHeader.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { View, StyleSheet, StatusBar } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
+import Text from "./Text";
+import FilterDropdown from "./FilterDropdown";
 import {
   headerBgColor,
   filterButtonColor,
@@ -8,15 +10,33 @@ import {
 } from "../constants/colors";
 import text from "../constants/text";
 
+const OPTIONS_DAY = [
+  "Any day",
+  "Today",
+  "Tomorrow",
+  "This weekend",
+  "Next week",
+  "Choose dates"
+];
+const OPTIONS_TIME = ["Any time", "Morning", "Afternoon", "Evening"];
+
 const FilterHeader = () => (
   <SafeAreaView style={styles.container} forceInset={{ top: "always" }}>
     <StatusBar barStyle="light-content" animated />
     <View style={styles.content}>
-      <View style={styles.filterButton}>
-        <Text style={styles.filterButtonText}>{text.filterButton}</Text>
+      <View style={styles.contentSearch}>
+        <View style={styles.filterButton}>
+          <Text type="h2" style={styles.filterButtonText}>
+            {text.filterButton}
+          </Text>
+        </View>
+        <View style={styles.mapButton}>
+          <Text style={styles.mapButtonText}>Map</Text>
+        </View>
       </View>
-      <View style={styles.mapButton}>
-        <Text style={styles.mapButtonText}>Map</Text>
+      <View style={styles.contentFilters}>
+        <FilterDropdown options={OPTIONS_DAY} />
+        <FilterDropdown options={OPTIONS_TIME} />
       </View>
     </View>
   </SafeAreaView>
@@ -27,20 +47,22 @@ const styles = StyleSheet.create({
     backgroundColor: headerBgColor
   },
   content: {
-    alignItems: "center",
-    flexDirection: "row",
     height: 169,
-    paddingTop: 8,
+    paddingTop: 12,
     paddingBottom: 12
+  },
+  contentSearch: {
+    alignItems: "center",
+    flexDirection: "row"
   },
   filterButton: {
     flex: 1,
-    flexDirection: "row",
     height: 44,
     backgroundColor: filterButtonColor,
     marginLeft: 8,
+    paddingHorizontal: 12,
     borderRadius: 4,
-    alignItems: "center"
+    justifyContent: "center"
   },
   mapButton: {
     marginHorizontal: 12,
@@ -52,16 +74,17 @@ const styles = StyleSheet.create({
     borderRadius: 25
   },
   filterButtonText: {
-    color: filterButtontextColor,
-    fontFamily: "Poppins-Bold",
-    fontSize: 18,
-    margin: 10
+    color: filterButtontextColor
   },
   mapButtonText: {
     color: filterButtontextColor,
     fontFamily: "Poppins-Bold",
     fontSize: 14,
     paddingBottom: 6
+  },
+  contentFilters: {
+    marginLeft: 8,
+    flexDirection: "row"
   }
 });
 
