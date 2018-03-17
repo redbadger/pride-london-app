ce91ba81c629253ba772efa4755a3f7c4717e430
Tracy Wu <sytracywu@gmail.com>
2018-03-17 20:11:24 +0000

    fixed snapshot tests

--- src/components/EventCard.test.js
+++ src/components/EventCard.test.js
@@ -3,7 +3,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import EventCard from "./EventCard";
 
-it("renders correctly", () => {
+it("renders correctly when isFree is true", () => {
   const output = shallow(
     <EventCard
       name="name"
@@ -11,8 +11,36 @@ it("renders correctly", () => {
       startTime="2018-09-16T12:12:12+01:00"
       endTime="2018-09-16T13:12:12+01:00"
       isFree
+      eventPriceLow={0}
+      eventPriceHigh={0}
+    />
+  );
+  expect(output).toMatchSnapshot();
+});
+it("renders correctly when isFree is false and there is a price range", () => {
+  const output = shallow(
+    <EventCard
+      name="name"
+      locationName="location"
+      startTime="2018-09-16T12:12:12+01:00"
+      endTime="2018-09-16T13:12:12+01:00"
+      isFree={false}
       eventPriceLow={12}
       eventPriceHigh={14}
+    />
+  );
+  expect(output).toMatchSnapshot();
+});
+it("renders correctly when isFree is false and there's only one price", () => {
+  const output = shallow(
+    <EventCard
+      name="name"
+      locationName="location"
+      startTime="2018-09-16T12:12:12+01:00"
+      endTime="2018-09-16T13:12:12+01:00"
+      isFree={false}
+      eventPriceLow={12}
+      eventPriceHigh={12}
     />
   );
   expect(output).toMatchSnapshot();
