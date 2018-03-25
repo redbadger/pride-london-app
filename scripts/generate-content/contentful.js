929d52e865cc2f4dc54446f8f31336c68c72cab2
Robbie McCorkell <robbiemccorkell@gmail.com>
2018-03-25 17:33:16 +0100

    Convert whole functions to async

--- scripts/generate-content/contentful.js
+++ scripts/generate-content/contentful.js
@@ -4,79 +4,81 @@ const { generateEvent } = require("./generators");
 
 const MAX_EVENTS = 150;
 
-const generateEvents = (spaceId, accessToken, count) => {
+const generateEvents = async (spaceId, accessToken, count) => {
   const client = contentful.createClient({
     accessToken
   });
 
-  client
-    .getSpace(spaceId)
-    .then(async space => {
-      const eventType = await space.getContentType("event");
-      const assets = await space.getAssets();
-      const { total: totalEvents } = await space.getEntries({
-        content_type: "event",
-        limit: 0
-      });
-      const toGenerate =
-        count + totalEvents >= MAX_EVENTS ? MAX_EVENTS - totalEvents : count;
+  try {
+    const space = await client.getSpace(spaceId);
 
-      console.log(`Space contains ${totalEvents} events`);
-      console.log(`Generating ${toGenerate} events`);
+    const eventType = await space.getContentType("event");
+    const assets = await space.getAssets();
+    const { total: totalEvents } = await space.getEntries({
+      content_type: "event",
+      limit: 0
+    });
+    const toGenerate =
+      count + totalEvents >= MAX_EVENTS ? MAX_EVENTS - totalEvents : count;
 
-      const eventCategories = eventType.fields.find(
-        field => field.id === "eventCategories"
-      ).items.validations[0].in;
+    console.log(`Space contains ${totalEvents} events`);
+    console.log(`Generating ${toGenerate} events`);
 
-      const newEntries = Array.from(Array(toGenerate)).map(() =>
-        generateEvent(eventCategories, assets)
-      );
+    const eventCategories = eventType.fields.find(
+      field => field.id === "eventCategories"
+    ).items.validations[0].in;
 
-      const newEntryPromises = newEntries.map(entry =>
-        space
-          .createEntry("event", entry)
-          .then(createdEntry => createdEntry.publish())
-      );
+    const newEntries = Array.from(Array(toGenerate)).map(() =>
+      generateEvent(eventCategories, assets)
+    );
 
-      await Promise.all(newEntryPromises);
-    })
-    .catch(console.error);
+    const newEntryPromises = newEntries.map(entry =>
+      space
+        .createEntry("event", entry)
+        .then(createdEntry => createdEntry.publish())
+    );
+
+    await Promise.all(newEntryPromises);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-const deleteGeneratedEvents = (spaceId, accessToken) => {
+const deleteGeneratedEvents = async (spaceId, accessToken) => {
   const client = contentful.createClient({
     accessToken
   });
 
-  client
-    .getSpace(spaceId)
-    .then(async space => {
-      const entries = await space.getEntries({
-        content_type: "event",
-        "fields.name[match]": "Generated: ",
-        select: "sys.id"
-      });
+  const space = await client.getSpace(spaceId);
 
-      const entryIds = entries.items.map(entry => entry.sys.id);
+  try {
+    const entries = await space.getEntries({
+      content_type: "event",
+      "fields.name[match]": "Generated: ",
+      select: "sys.id"
+    });
 
-      console.log(`Space contains ${entries.total} generated events`);
-      console.log(`Deleting ${entryIds.length} events`);
+    const entryIds = entries.items.map(entry => entry.sys.id);
 
-      const deletionPromises = entryIds.map(entryId =>
-        space
-          .getEntry(entryId)
-          .then(entry => {
-            if (entry.isPublished()) {
-              return entry.unpublish();
-            }
-            return entry;
-          })
-          .then(entry => entry.delete())
-      );
+    console.log(`Space contains ${entries.total} generated events`);
+    console.log(`Deleting ${entryIds.length} events`);
 
-      await Promise.all(deletionPromises);
-    })
-    .catch(console.error);
+    const deletionPromises = entryIds.map(entryId =>
+      space
+        .getEntry(entryId)
+        .then(entry => {
+          if (entry.isPublished()) {
+            return entry.unpublish();
+          }
+          return entry;
+        })
+        .then(entry => entry.delete())
+    );
+
+    await Promise.all(deletionPromises);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 module.exports = {
