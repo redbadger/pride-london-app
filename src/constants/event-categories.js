cf8b1f5825c2b6d49f88b17e0e81344724be1caa
RGBboy <l-_-l@rgbboy.com>
2018-03-25 13:34:08 +0100

    add initial event categories constants;

---
+++ src/constants/event-categories.js
@@ -0,0 +1,38 @@
+// Ideally the keys here should be consts, but currently these are setup in contentful only as the label, hence we need to use this for the key too. If we move to realise categories as its own content type.
+const categories = {
+  "Cabaret and Variety": {
+    label: "Cabaret and Variety"
+  },
+  Community: {
+    label: "Community"
+  },
+  "Talks and Debates": {
+    label: "Talks and Debates"
+  },
+  "Film and Screenings": {
+    label: "Film and Screenings"
+  },
+  "Plays and Theatre": {
+    label: "Plays and Theatre"
+  },
+  "Social and Networking": {
+    label: "Social and Networking"
+  },
+  Nightlife: {
+    label: "Nightlife"
+  },
+  "Exhibition and Tours": {
+    label: "Exhibition and Tours"
+  },
+  "Sports and Activities": {
+    label: "Sports and Activities"
+  },
+  Health: {
+    label: "Health"
+  },
+  Music: {
+    label: "Music"
+  }
+};
+
+export default categories;
