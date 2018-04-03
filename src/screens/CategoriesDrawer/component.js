d05419f1cb7c1216965d1e89a13a1ab84940d5c4
joaquimadraz <joaquim.adraz@gmail.com>
2018-04-03 14:37:50 +0100

    Connect categories screen to store

---
+++ src/screens/CategoriesDrawer/component.js
@@ -0,0 +1,131 @@
+// @flow
+import React, { PureComponent } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import SafeAreaView from "react-native-safe-area-view";
+import type { NavigationScreenProp, NavigationState } from "react-navigation";
+import type { Event } from "../../data/event";
+import Text from "../../components/Text";
+import {
+  categoriesDrawerBgColor,
+  categoriesDrawerCancelColor,
+  whiteColor,
+  darkBlueGreyTwoColor,
+  eucalyptusGreenColor,
+  lightNavyBlueColor
+} from "../../constants/colors";
+import text from "../../constants/text";
+
+import ContentPadding from "../../components/ContentPadding";
+
+type Props = {
+  navigation: NavigationScreenProp<NavigationState>,
+  events: Event[]
+};
+
+class CategoriesDrawer extends PureComponent<Props> {
+  static navigationOptions = {
+    header: null,
+    tabBarVisible: false
+  };
+
+  handleClose = () => {
+    this.props.navigation.goBack();
+  };
+
+  handleShowCategories = () => {
+    // TODO
+  };
+
+  render() {
+    const { events } = this.props;
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <ContentPadding style={styles.contents}>
+          <View>
+            <TouchableOpacity
+              style={styles.cancelButton}
+              onPress={this.handleClose}
+            >
+              <Text type="h4" style={styles.cancelButtonText}>
+                {text.cancel}
+              </Text>
+            </TouchableOpacity>
+            <Text type="h1" style={styles.filterTitle}>
+              {text.filterTitle}
+            </Text>
+            <View style={styles.selectedCategoriesPills}>
+              <Text type="h2" style={styles.zeroSelected}>
+                {text.zeroSelected}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.categoriesList} />
+          <View>
+            <TouchableOpacity
+              style={styles.showEventsButton}
+              onPress={this.handleShowCategories}
+            >
+              <Text type="h2" style={styles.showEventsText}>
+                {text.showEvents(events.length)}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ContentPadding>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: categoriesDrawerBgColor
+  },
+  contents: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  cancelButton: {
+    marginTop: 10
+  },
+  cancelButtonText: {
+    color: categoriesDrawerCancelColor
+  },
+  filterTitle: {
+    color: whiteColor,
+    marginTop: 14,
+    marginBottom: 6
+  },
+  selectedCategoriesPills: {
+    backgroundColor: darkBlueGreyTwoColor,
+    paddingTop: 11,
+    paddingBottom: 9,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 4,
+    marginBottom: 16
+  },
+  zeroSelected: {
+    color: eucalyptusGreenColor
+  },
+  categoriesList: {
+    backgroundColor: eucalyptusGreenColor,
+    flex: 1
+  },
+  showEventsButton: {
+    backgroundColor: eucalyptusGreenColor,
+    width: "100%",
+    paddingTop: 13,
+    paddingBottom: 11,
+    borderRadius: 4,
+    marginTop: 16,
+    marginBottom: 16
+  },
+  showEventsText: {
+    color: lightNavyBlueColor,
+    textAlign: "center"
+  }
+});
+
+export default CategoriesDrawer;
--- src/screens/CategoriesDrawer/index.js
+++ src/screens/CategoriesDrawer/index.js
@@ -1,127 +1,24 @@
 // @flow
-import React, { PureComponent } from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
-import type { NavigationScreenProp, NavigationState } from "react-navigation";
-import SafeAreaView from "react-native-safe-area-view";
-import Text from "../../components/Text";
-import {
-  categoriesDrawerBgColor,
-  categoriesDrawerCancelColor,
-  whiteColor,
-  darkBlueGreyTwoColor,
-  eucalyptusGreenColor,
-  lightNavyBlueColor
-} from "../../constants/colors";
-import text from "../../constants/text";
+import { connect } from "react-redux";
+import type { Connector, MapStateToProps } from "react-redux";
+import type { NavigationScreenProp } from "react-navigation";
+import type { Event } from "../../data/event";
+import type { State } from "../../reducers";
+import { selectFilteredEvents } from "../../selectors/events";
+import Component from "./component";
 
-import ContentPadding from "../../components/ContentPadding";
-
-type Props = {
-  navigation: NavigationScreenProp<NavigationState>
+type OwnProps = {
+  navigation: NavigationScreenProp<*>
 };
 
-class CategoriesDrawer extends PureComponent<Props> {
-  static navigationOptions = {
-    header: null,
-    tabBarVisible: false
-  };
+type Props = {
+  events: Event[]
+} & OwnProps;
 
-  handleClose = () => {
-    this.props.navigation.pop();
-  };
-
-  handleShowCategories = () => {
-    // TODO
-  };
-
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <ContentPadding style={styles.contents}>
-          <View>
-            <TouchableOpacity
-              style={styles.cancelButton}
-              onPress={this.handleClose}
-            >
-              <Text type="h4" style={styles.cancelButtonText}>
-                {text.cancel}
-              </Text>
-            </TouchableOpacity>
-            <Text type="h1" style={styles.filterTitle}>
-              {text.filterTitle}
-            </Text>
-            <View style={styles.selectedCategoriesPills}>
-              <Text type="h2" style={styles.zeroSelected}>
-                {text.zeroSelected}
-              </Text>
-            </View>
-          </View>
-          <View style={styles.categoriesList} />
-          <View>
-            <TouchableOpacity
-              style={styles.showEventsButton}
-              onPress={this.handleShowCategories}
-            >
-              <Text type="h2" style={styles.showEventsText}>
-                {text.showEvents(213)}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </ContentPadding>
-      </SafeAreaView>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: categoriesDrawerBgColor
-  },
-  contents: {
-    flex: 1,
-    justifyContent: "space-between"
-  },
-  cancelButton: {
-    marginTop: 10
-  },
-  cancelButtonText: {
-    color: categoriesDrawerCancelColor
-  },
-  filterTitle: {
-    color: whiteColor,
-    marginTop: 14,
-    marginBottom: 6
-  },
-  selectedCategoriesPills: {
-    backgroundColor: darkBlueGreyTwoColor,
-    paddingTop: 11,
-    paddingBottom: 9,
-    paddingLeft: 16,
-    paddingRight: 16,
-    borderRadius: 4,
-    marginBottom: 16
-  },
-  zeroSelected: {
-    color: eucalyptusGreenColor
-  },
-  categoriesList: {
-    backgroundColor: eucalyptusGreenColor,
-    flex: 1
-  },
-  showEventsButton: {
-    backgroundColor: eucalyptusGreenColor,
-    width: "100%",
-    paddingTop: 13,
-    paddingBottom: 11,
-    borderRadius: 4,
-    marginTop: 16,
-    marginBottom: 16
-  },
-  showEventsText: {
-    color: lightNavyBlueColor,
-    textAlign: "center"
-  }
+const mapStateToProps: MapStateToProps<State, OwnProps, *> = state => ({
+  events: selectFilteredEvents(state)
 });
 
-export default CategoriesDrawer;
+const connector: Connector<OwnProps, Props> = connect(mapStateToProps);
+
+export default connector(Component);
