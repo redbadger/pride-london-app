73602d3c2455343a7cee700054cd9c95cac96615
Peter Kadlot <strand.es.verfurdo@gmail.com>
2018-04-04 09:21:45 +0100

    add scroll view

--- src/components/CategoriesDrawerList.js
+++ src/components/CategoriesDrawerList.js
@@ -1,121 +1,58 @@
 // @flow
 import React from "react";
-import { StyleSheet, SectionList, TouchableOpacity, View } from "react-native";
-import type { SectionBase } from "react-native/Libraries/Lists/SectionList";
-import formatDate from "date-fns/format";
-
+import { StyleSheet, ScrollView, TouchableOpacity, View } from "react-native";
 import ContentPadding from "./ContentPadding";
 import Text from "./Text";
-import type { Category, LocalizedFieldRef } from "../data/event";
-import { filterBgColor } from "../constants/colors";
+import type { EventCategoryList } from "../data/event";
+import { filterBgColor, whiteColor } from "../constants/colors";
 
 type Props = {
-  locale: string,
-  events: EventDays,
-  refreshing?: boolean,
-  onRefresh?: () => void,
-  onPress: (eventName: string) => void,
-  getAssetUrl: LocalizedFieldRef => string
+  categories: EventCategoryList
 };
 
-const separator = style => () => <View style={style} />;
-
-type ItemProps = { item: Event };
-const renderItem = (styles, locale, onPress, getAssetUrl) => ({
-  item: event
-}: ItemProps) => (
-  <ContentPadding>
-    <TouchableOpacity
-      style={styles.eventListItem}
-      accessibilityTraits={["button"]}
-      accessibilityComponentType="button"
-      delayPressIn={50}
-      onPress={() => onPress(event.sys.id)}
-    />
-  </ContentPadding>
+const CategoriesDrawerList = ({ categories }: Props) => (
+  <ScrollView style={styles.container}>
+    {categories.map(category => (
+      <TouchableOpacity
+        key={category.label}
+        style={styles.itemContainer}
+        accessibilityTraits={["button"]}
+        accessibilityComponentType="button"
+        delayPressIn={50}
+      >
+        <View
+          style={{ height: 48, width: 16, backgroundColor: category.color }} // eslint-disable-line react-native/no-inline-styles
+        />
+        <ContentPadding style={styles.item}>
+          <Text style={styles.itemText}>{category.label}</Text>
+        </ContentPadding>
+      </TouchableOpacity>
+    ))}
+  </ScrollView>
 );
-
-type Section = SectionBase<Category> & { title: string };
-
-// type SectionProps = { section: Section };
-// const renderSectionHeader = styles => ({ section }: SectionProps) => (
-//   <ContentPadding style={styles.sectionHeader}>
-//     <Text type="h2" style={styles.sectionHeaderText}>
-//       {section.title}
-//     </Text>
-//   </ContentPadding>
-// );
-
-// const eventSections = (events: EventDays, locale: string): Section[] =>
-//   events.map(it => ({
-//     data: it,
-//     title: formatDate(it[0].fields.startTime[locale], "dddd D MMMM")
-//   }));
-
-const categorySections = (categories: Category, locale: string): Section[] =>
-  events.map(it => ({
-    data: it,
-    title: formatDate(it[0].fields.startTime[locale], "dddd D MMMM")
-  }));
-
-const EventList = ({
-  events,
-  locale,
-  refreshing,
-  onRefresh,
-  onPress,
-  getAssetUrl
-}: Props) => (
-  <SectionList
-    // stickySectionHeadersEnabled
-    sections={eventSections(events, locale)}
-    // renderSectionHeader={renderSectionHeader(styles)}
-    // renderSectionFooter={separator(styles.sectionFooter)}
-    renderItem={renderItem(styles, locale, onPress, getAssetUrl)}
-    keyExtractor={event => event.sys.id}
-    contentContainerStyle={styles.container}
-    ItemSeparatorComponent={separator(styles.itemSeparator)}
-    SectionSeparatorComponent={separator(styles.sectionSeparator)}
-    refreshing={refreshing}
-    onRefresh={onRefresh}
-  />
-);
-
-EventList.defaultProps = {
-  refreshing: false,
-  onRefresh: undefined
-};
 
 const styles = StyleSheet.create({
-  itemSeparator: {
-    height: 12
-  },
-  sectionSeparator: {
-    height: 6
-  },
   container: {
-    // paddingTop: 0,
     backgroundColor: filterBgColor
   },
-  eventListItem: {
-    // borderRadius: 5
-    // // The below properties are required for ioS shadow
-    // shadowOffset: { width: 0, height: 1 },
-    // shadowOpacity: 1,
-    // shadowRadius: 3,
-    // // The below properties are required for android shadow
-    // borderWidth: 0,
-    // elevation: 3,
-    // backgroundColor: bgColor
+  itemContainer: {
+    flex: 1,
+    flexDirection: "row",
+    marginBottom: 8,
+    paddingTop: 8,
+    paddingBottom: 12,
+    width: 339,
+    height: 48
+  },
+  item: {},
+  itemText: {
+    color: whiteColor,
+    textAlign: "left",
+    fontFamily: "Poppins-Bold",
+    fontSize: 24,
+    lineHeight: 28,
+    letterSpacing: 0
   }
-  //   sectionHeader: {
-  //     height: 40,
-  //     justifyContent: "center"
-  //   },
-  //   sectionHeaderText: {},
-  //   sectionFooter: {
-  //     height: 6
-  //   }
 });
 
-export default EventList;
+export default CategoriesDrawerList;
