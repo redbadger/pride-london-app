d9adbc6573b47a6ea482796411739eb14eaf3c46
joaquimadraz <joaquim.adraz@gmail.com>
2018-04-05 10:35:08 +0100

    Animate top and bottom shadows depending on the scroll position

--- src/components/ShadowedScrollView.js
+++ src/components/ShadowedScrollView.js
@@ -9,15 +9,66 @@ type Props = {
   style: StyleObj
 };
 
-class ShadowedScrollView extends React.PureComponent<Props> {
+type State = {
+  topShadowOpacity: number,
+  bottomShadowOpacity: number
+};
+
+const maxShadowOpacity = 0.7;
+
+class ShadowedScrollView extends React.PureComponent<Props, State> {
+  constructor() {
+    super();
+
+    this.state = { topShadowOpacity: 0, bottomShadowOpacity: maxShadowOpacity };
+  }
+
+  handleScroll = (event: Object) => {
+    const maxScrollOffset = this.contentViewHeight - this.scrollViewHeight;
+    const currentScrollOffset = event.nativeEvent.contentOffset.y;
+    const rawOpacity = currentScrollOffset * maxShadowOpacity / maxScrollOffset;
+
+    const precision = 2;
+    const factor = Math.pow(10, precision);
+    const opacity = Math.round(rawOpacity * factor) / factor;
+    const toppedOpacity =
+      rawOpacity > maxShadowOpacity
+        ? maxShadowOpacity
+        : opacity <= 0 ? 0 : opacity;
+
+    this.setState({
+      topShadowOpacity: toppedOpacity,
+      bottomShadowOpacity: maxShadowOpacity - toppedOpacity
+    });
+  };
+
+  handleScrollViewLayout = (event: Object) => {
+    const { height } = event.nativeEvent.layout;
+    this.scrollViewHeight = height;
+  };
+
+  handleContentViewLayout = (event: Object) => {
+    const { height } = event.nativeEvent.layout;
+    this.contentViewHeight = height;
+  };
+
   render() {
     const { style, children } = this.props;
+    const { topShadowOpacity, bottomShadowOpacity } = this.state;
 
     return (
       <View style={[styles.container, style]}>
-        <View style={styles.topShadow} />
-        <ScrollView>{children}</ScrollView>
-        <View style={styles.bottomShadow} />
+        <View style={[styles.topShadow, { shadowOpacity: topShadowOpacity }]} />
+        <ScrollView
+          onLayout={this.handleScrollViewLayout}
+          onScroll={this.handleScroll}
+          scrollEventThrottle={16}
+        >
+          <View onLayout={this.handleContentViewLayout}>{children}</View>
+        </ScrollView>
+        <View
+          style={[styles.bottomShadow, { shadowOpacity: bottomShadowOpacity }]}
+        />
       </View>
     );
   }
