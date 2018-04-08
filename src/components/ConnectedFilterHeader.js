b1d457ebe79d5b2254ec0aa781d49cc69ee4f728
Jon Yardley <jon.yardley@red-badger.com>
2018-04-08 15:58:40 +0100

    Fix flow errors

--- src/components/ConnectedFilterHeader.js
+++ src/components/ConnectedFilterHeader.js
@@ -1,17 +1,12 @@
 // @flow
 import { connect } from "react-redux";
 import type { Connector } from "react-redux";
-import type { DateRange, Time } from "../data/date-time";
 import { selectDateFilter, selectTimeFilter } from "../selectors/event-filters";
+import type { Props as ComponentProps } from "./FilterHeader";
 import Component from "./FilterHeader";
 
 type OwnProps = {};
-
-type Props = {
-  dateFilter: ?DateRange,
-  timeFilter: Set<Time>,
-  selectedCategories: Set<string>
-} & OwnProps;
+type Props = ComponentProps & OwnProps;
 
 const mapStateToProps = state => ({
   dateFilter: selectDateFilter(state),
