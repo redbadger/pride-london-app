84a0d21837d9db613aa21d7cf675b461421c6faa
Peter Kadlot <strand.es.verfurdo@gmail.com>
2018-04-12 17:49:58 +0100

    a few more tests

--- src/screens/CategoriesFilterScreen/ListItem.test.js
+++ src/screens/CategoriesFilterScreen/ListItem.test.js
@@ -1,5 +1,6 @@
 // @flow
 import React from "react";
+import { View } from "react-native";
 import { shallow } from "enzyme";
 import ListItem from "./ListItem";
 import type { EventCategory } from "../../data/event";
@@ -16,5 +17,27 @@ describe("ListItem Component", () => {
     const component = render({ category, selected: true });
 
     expect(component).toMatchSnapshot();
+  });
+
+  it("renders correctly without selected", () => {
+    const category: EventCategory = { label: "Music", color: "#ffffff" };
+    const component = render({ category, selected: false });
+
+    expect(component).toMatchSnapshot();
+  });
+
+  describe("#handleOnLayout", () => {
+    it("stores the height and width of the component", () => {
+      const category: EventCategory = { label: "Music", color: "#ffffff" };
+      const component = render({ category, selected: false });
+      component
+        .find(View)
+        .props()
+        .onLayout({
+          nativeEvent: { layout: { width: 100, height: 200 } }
+        });
+      expect(component.state("textWidth")).toEqual(100);
+      expect(component.state("textHeight")).toEqual(200);
+    });
   });
 });
