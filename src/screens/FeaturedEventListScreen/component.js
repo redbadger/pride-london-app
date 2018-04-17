94af346cb7ac97b0cac1fe43bdbbe2278c2f5d28
RGBboy <l-_-l@rgbboy.com>
2018-04-17 12:07:01 +0100

    Add header to FeaturedEvents screen;

--- src/screens/FeaturedEventListScreen/component.js
+++ src/screens/FeaturedEventListScreen/component.js
@@ -3,9 +3,15 @@ import React, { PureComponent } from "react";
 import { StyleSheet, View } from "react-native";
 import type { NavigationScreenProp } from "react-navigation";
 import type { EventDays, LocalizedFieldRef } from "../../data/event";
+import ContentPadding from "../../components/ContentPadding";
 import EventList from "../../components/EventList";
-import { bgColor } from "../../constants/colors";
+import Header from "../../components/Header";
+import IconButton from "../../components/IconButton";
+import Text from "../../components/Text";
+import { bgColor, whiteColor } from "../../constants/colors";
 import { EVENT_DETAILS } from "../../constants/routes";
+import text from "../../constants/text";
+import chevronLeftWhite from "../../../assets/images/chevron-left-white.png";
 
 import locale from "../../data/locale";
 
@@ -16,9 +22,29 @@ type Props = {
 };
 
 class EventsScreen extends PureComponent<Props> {
+  static navigationOptions = {
+    header: null,
+    tabBarVisible: false
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <Header>
+          <ContentPadding style={styles.headerContent}>
+            <IconButton
+              accessibilityLabel="Back"
+              onPress={() => {
+                this.props.navigation.goBack(null);
+              }}
+              source={chevronLeftWhite}
+            />
+            <Text type="h2" style={styles.headerTitle}>
+              {text.featuredEventListTitle}
+            </Text>
+            <View style={styles.phantomIcon} />
+          </ContentPadding>
+        </Header>
         <EventList
           locale={locale}
           events={this.props.events}
@@ -36,6 +62,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: bgColor
+  },
+  headerContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  headerTitle: {
+    color: whiteColor
+  },
+  phantomIcon: {
+    width: 48,
+    height: 48
   }
 });
 
