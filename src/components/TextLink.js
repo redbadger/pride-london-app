a64d28f746ee2f82709271775ae85b6758759f3f
Matt Paul <mattpaul001@icloud.com>
2018-04-18 17:35:20 +0100

    Create TextLink component

---
+++ src/components/TextLink.js
@@ -0,0 +1,34 @@
+// @flow
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Text from "./Text";
+import { eucalyptusGreenColor } from "../constants/colors";
+import type { TextType } from "./Text";
+
+type Props = {
+  children: string,
+  type?: TextType
+};
+
+const TextLink = ({ children, type }: Props) => (
+  <View style={styles.detailTitleLink}>
+    <Text type={type} blue>
+      {children}
+    </Text>
+  </View>
+);
+
+TextLink.defaultProps = {
+  type: "h4"
+};
+
+const styles = StyleSheet.create({
+  detailTitleLink: {
+    borderBottomColor: eucalyptusGreenColor,
+    borderBottomWidth: StyleSheet.hairlineWidth * 2,
+    alignSelf: "flex-start",
+    marginBottom: 2
+  }
+});
+
+export default TextLink;
