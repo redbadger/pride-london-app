2b4a49e8a68b3191d2bf87832e0c302539c0b892
RGBboy <l-_-l@rgbboy.com>
2018-04-18 10:52:53 +0100

    use color const for shadow;

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -48,7 +48,7 @@ export const tabBarBgColor = "rgb(243, 243, 243)";
 export const tabBarLabelColor = "rgb(88, 88, 88)";
 export const tabBarActiveLineColor = "rgb(44, 218, 157)";
 export const tabBarActiveLabelColor = "rgb(45, 47, 127)";
-export const tabBarShadowColor = "rgba(0, 0, 0, 0.1)";
+export const tabBarShadowColor = shadowColor;
 export const tabBarBorderColor = "rgba(0, 0, 0, 0.25)";
 
 // FILTER HEADER
