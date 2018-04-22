12cd6ed1b36031b2d997a80be6f38ce9de0ef6f2
Mark Holland <mark@partiallogic.com>
2018-04-22 17:03:54 +0100

    reduces e2e Jest timeout

--- e2e/init.js
+++ e2e/init.js
@@ -1,8 +1,8 @@
 import detox from "detox";
 import { detox as config } from "../package.json";
 
-// Set the default test timeout of 120s
-jest.setTimeout(300000);
+// Set the default test timeout of 5s
+jest.setTimeout(5000);
 
 beforeAll(async () => {
   await detox.init(config);
