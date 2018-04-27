d7022fb001e12a4d2a7536568bc2a7396aafe1a9
RGBboy <l-_-l@rgbboy.com>
2018-04-27 10:41:05 +0100

    add saved events reducer;

---
+++ src/reducers/saved-events.js
@@ -0,0 +1,31 @@
+// @flow
+import type { Reducer } from "redux";
+import type { SaveEventAction } from "../actions/saved-events";
+
+type State = Set<string>;
+
+const defaultState = new Set();
+
+const savedEvents: Reducer<State, SaveEventAction> = (
+  state: State = defaultState,
+  action: SaveEventAction
+) => {
+  switch (action.type) {
+    case "ADD_SAVED_EVENT":
+      if (!state.has(action.id)) {
+        return new Set(state.values()).add(action.id);
+      }
+      return state;
+    case "REMOVE_SAVED_EVENT":
+      if (state.has(action.id)) {
+        const newState = new Set(state.values());
+        newState.delete(action.id);
+        return newState;
+      }
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default savedEvents;
