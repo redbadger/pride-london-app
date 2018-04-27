abdb33d204ad4d93c58e9713f74d76b4513c013f
Mark Holland <mark@partiallogic.com>
2018-04-27 18:56:45 +0100

    set timeout to 20s

--- e2e/init.js
+++ e2e/init.js
@@ -1,8 +1,8 @@
 import detox from "detox";
 import { detox as config } from "../package.json";
 
-// Set the default test timeout of 10s
-jest.setTimeout(10000);
+// Set the default test timeout of 20s
+jest.setTimeout(20000);
 
 beforeAll(async () => {
   await detox.init(config);
