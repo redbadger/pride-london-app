5f68986dfeea97d3b0abfe9cd564c0f651c5de3b
RGBboy <l-_-l@rgbboy.com>
2018-04-27 11:11:43 +0100

    add eventIsSaved selector;

--- src/selectors/event.js
+++ src/selectors/event.js
@@ -1,8 +1,11 @@
 // @flow
 import type { Event } from "../data/event";
+import type { State as SavedEventsState } from "../reducers/saved-events";
 import locale from "../data/locale";
 
-// eslint-disable-next-line import/prefer-default-export
 export const selectEventIsFree = (event: Event) =>
   event.fields.eventPriceLow[locale] === 0 &&
   event.fields.eventPriceHigh[locale] === 0;
+
+export const eventIsSaved = (savedEvents: SavedEventsState, event: Event) =>
+  savedEvents.has(event.sys.id);
--- src/selectors/event.test.js
+++ src/selectors/event.test.js
@@ -1,5 +1,5 @@
 // @flow
-import { selectEventIsFree } from "./event";
+import { eventIsSaved, selectEventIsFree } from "./event";
 import type { Event } from "../data/event";
 
 describe("selectEventIsFree", () => {
@@ -23,5 +23,31 @@ describe("selectEventIsFree", () => {
     }: any): Event);
 
     expect(selectEventIsFree(event)).toBe(false);
+  });
+});
+
+describe("eventIsSaved", () => {
+  it("returns true if event id exists in savedEvents", () => {
+    const id = "9df600f3-12d0-4611-a5e3-ace6219ad163";
+    const event = (({
+      sys: {
+        id
+      }
+    }: any): Event);
+    const savedEvents = new Set([id]);
+
+    expect(eventIsSaved(savedEvents, event)).toBe(true);
+  });
+
+  it("returns false if event id does not exist in savedEvents", () => {
+    const id = "179acbdf-61d1-4fae-b005-5f4a359a7c85";
+    const event = (({
+      sys: {
+        id
+      }
+    }: any): Event);
+    const savedEvents = new Set();
+
+    expect(eventIsSaved(savedEvents, event)).toBe(false);
   });
 });
