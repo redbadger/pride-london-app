648f940f3e0aa54615ce47681552973deb167f73
RGBboy <l-_-l@rgbboy.com>
2018-05-01 14:50:10 +0100

    fix up the ol tests;

--- src/components/SaveEventButton.test.js
+++ src/components/SaveEventButton.test.js
@@ -68,7 +68,7 @@ describe("update from inactive to active", () => {
     output.setState({ progress: new Animated.Value(0) });
     output.setProps({ active: true, onPress });
     expect(Animated.timing).toBeCalledWith(mockAnimatedValue, {
-      duration: 1920,
+      duration: 760,
       toValue: 1,
       easing: Easing.linear,
       useNativeDriver: true
