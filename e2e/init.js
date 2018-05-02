18defceb51e71814ac9c6c9f7c18f5127f0d440c
Mark Holland <mark.holland@red-badger.com>
2018-05-02 09:23:35 +0100

    undo changes

--- e2e/init.js
+++ e2e/init.js
@@ -4,13 +4,8 @@ import { detox as config } from "../package.json";
 // Set the default test timeout of 20s
 jest.setTimeout(20000);
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 beforeAll(async () => {
   await detox.init(config);
-  await sleep(1000);
 });
 
 afterAll(async () => {
