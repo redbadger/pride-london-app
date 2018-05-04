359b9684aaf904af12431842dcc8b24090bc8450
Zoe Bryant <zoeabryant@gmail.com>
2018-05-04 17:11:52 +0100

    If 'High' and 'Low' pricing are both '£0' the event should be treated as if it is marked 'Free'

--- src/selectors/event.js
+++ src/selectors/event.js
@@ -4,5 +4,6 @@ import locale from "../data/locale";
 
 // eslint-disable-next-line import/prefer-default-export
 export const selectEventIsFree = (event: Event) =>
-  event.fields.eventPriceLow[locale] === 0 &&
-  event.fields.eventPriceHigh[locale] === 0;
+  event.fields.isFree[locale] ||
+  (event.fields.eventPriceLow[locale] === 0 &&
+    event.fields.eventPriceHigh[locale] === 0);
