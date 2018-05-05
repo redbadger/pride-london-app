30825761416b41c6abc20329d628ed7fce428282
Viktor Charypar <charypar@gmail.com>
2018-05-05 18:20:29 +0100

    Add a few tests

--- src/reducers/event-filters.test.js
+++ src/reducers/event-filters.test.js
@@ -9,6 +9,61 @@ describe("Event filters reducer", () => {
     expect(state).toMatchSnapshot();
   });
 
+  it("updates state with filters from payload for SET_EVENT_FILTERS action", () => {
+    const initialState = {
+      selectedFilters: {
+        categories: new Set(),
+        date: null,
+        timeOfDay: new Set(),
+        price: new Set(),
+        audience: new Set(),
+        venueDetails: new Set(),
+        accessibilityOptions: new Set(),
+        area: new Set()
+      },
+      stagedFilters: {
+        categories: new Set(),
+        date: null,
+        timeOfDay: new Set(),
+        price: new Set(),
+        audience: new Set(),
+        venueDetails: new Set(),
+        accessibilityOptions: new Set(),
+        area: new Set()
+      }
+    };
+    const state = reducer(initialState, {
+      type: "SET_EVENT_FILTERS",
+      payload: {
+        date: { startDate: "2018-03-12", endDate: "2018-03-12" }
+      }
+    });
+
+    expect(state.stagedFilters.categories).toBe(
+      initialState.stagedFilters.categories
+    );
+    expect(state.stagedFilters.date).toEqual({
+      startDate: "2018-03-12",
+      endDate: "2018-03-12"
+    });
+    expect(state.stagedFilters.timeOfDay).toBe(
+      initialState.stagedFilters.timeOfDay
+    );
+
+    expect(state.selectedFilters.categories).toBe(
+      initialState.stagedFilters.categories
+    );
+    expect(state.selectedFilters.date).toEqual({
+      startDate: "2018-03-12",
+      endDate: "2018-03-12"
+    });
+    expect(state.selectedFilters.timeOfDay).toBe(
+      initialState.stagedFilters.timeOfDay
+    );
+
+    expect(state.stagedFilters).toBe(state.selectedFilters);
+  });
+
   it("updates state with filters from payload for STAGE_EVENT_FILTERS action", () => {
     const initialState = {
       selectedFilters: {
