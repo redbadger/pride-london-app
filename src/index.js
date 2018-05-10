37657e265d2b0ce3e64c693657b4da4353391542
Mark Holland <mark.holland@red-badger.com>
2018-05-10 17:08:43 +0100

    remove alpha

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ bugsnagConfiguration.releaseStage = Config.RELEASE_STAGE
   ? Config.RELEASE_STAGE
   : "dev";
 // Only send reports for releases from master branch
-bugsnagConfiguration.notifyReleaseStages = ["alpha", "beta", "release"];
+bugsnagConfiguration.notifyReleaseStages = ["beta", "release"];
 const bugsnag = new Client(bugsnagConfiguration); // eslint-disable-line no-unused-vars
 
 // https://github.com/react-navigation/react-navigation/issues/3956#issuecomment-380648083
