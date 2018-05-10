a6eb944a809b27b80e0f286a694720ffb7ab7e8b
Mark Holland <mark.holland@red-badger.com>
2018-05-10 15:54:51 +0100

    allow alpha for testing

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ bugsnagConfiguration.releaseStage = Config.RELEASE_STAGE
   ? Config.RELEASE_STAGE
   : "dev";
 // Only send reports for releases from master branch
-bugsnagConfiguration.notifyReleaseStages = ["beta", "release"];
+bugsnagConfiguration.notifyReleaseStages = ["alpha", "beta", "release"];
 const bugsnag = new Client(bugsnagConfiguration); // eslint-disable-line no-unused-vars
 
 // https://github.com/react-navigation/react-navigation/issues/3956#issuecomment-380648083
