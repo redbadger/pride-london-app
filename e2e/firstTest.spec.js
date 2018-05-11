5d389eb52ca9b19d3a23e9fa4a6e8793621e68fb
Mark Holland <mark.holland@red-badger.com>
2018-05-11 15:49:46 +0100

    clean up

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,9 +3,6 @@ const { takeScreenshot } = require("./helpers");
 describe("Pride App", () => {
   beforeEach(async () => {
     await device.reloadReactNative();
-    await waitFor(element(by.id("home-screen")))
-      .toExist()
-      .withTimeout(200000);
   });
 
   afterEach(async () => {
@@ -13,35 +10,26 @@ describe("Pride App", () => {
   });
 
   it("should start on Home page", async () => {
-    const homeScreen = element(by.id("home-screen"));
-    await expect(homeScreen).toBeVisible();
+    await expect(element(by.id("home-screen"))).toBeVisible();
   });
 
   it("should navigate to Events tab", async () => {
-    const eventsTabButton = element(by.id("events-tab-button"));
-    await expect(eventsTabButton).toBeVisible();
     await element(by.id("events-tab-button")).tap();
     await expect(element(by.id("event-filter-header"))).toBeVisible();
   });
 
   it("should navigate to Parade tab", async () => {
-    const paradeTabButton = element(by.id("parade-tab-button"));
-    await expect(paradeTabButton).toBeVisible();
-    await paradeTabButton.tap();
+    await element(by.id("parade-tab-button")).tap();
     await expect(element(by.id("page-heading-parade"))).toBeVisible();
   });
 
   it("should navigate to Saved Events tab", async () => {
-    const savedEventsTabButton = element(by.id("saved-events-tab-button"));
-    await expect(savedEventsTabButton).toBeVisible();
-    await savedEventsTabButton.tap();
+    await element(by.id("saved-events-tab-button")).tap();
     await expect(element(by.id("page-heading-saved-events"))).toBeVisible();
   });
 
   it("should navigate to Support Us tab", async () => {
-    const supportUsTabButton = element(by.id("support-us-tab-button"));
-    await expect(supportUsTabButton).toBeVisible();
-    await supportUsTabButton.tap();
+    await element(by.id("support-us-tab-button")).tap();
     await expect(element(by.id("page-heading-support-us"))).toBeVisible();
   });
 });
