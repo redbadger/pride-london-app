555cd0251095b949192823c7e10cec37353f88fb
Zoe Bryant <zoeabryant@gmail.com>
2018-05-11 15:56:59 +0100

    Calendar should show last date picked, not start day

--- src/components/DateRangePicker.js
+++ src/components/DateRangePicker.js
@@ -109,7 +109,7 @@ class DateRangePicker extends React.PureComponent<Props> {
 
     return (
       <Calendar
-        current={dateRange ? dateRange.startDate : null}
+        current={dateRange ? dateRange.endDate : null}
         markedDates={getMarkedDates(dateRange, formatDate(today, "YYYY-MM-DD"))}
         markingType="period"
         onDayPress={this.onDaySelected}
