fddcb9122c3bb8cc8c183a78a1b8a253ba24ba8f
Jan Kuehle <jan.kuehle@red-badger.com>
2018-05-13 18:33:30 +0100

    Attempt to provide better accessibility for TextInput

--- src/screens/DonateScreen/NumberTextField.js
+++ src/screens/DonateScreen/NumberTextField.js
@@ -1,6 +1,14 @@
 // @flow
 import React from "react";
-import { PixelRatio, Platform, StyleSheet, TextInput } from "react-native";
+import {
+  PixelRatio,
+  Platform,
+  StyleSheet,
+  TextInput,
+  View
+} from "react-native";
+import type { ViewStyleProp } from "react-native/Libraries/StyleSheet/StyleSheet";
+import Text from "../../components/Text";
 import {
   lightNavyBlueColor,
   transparent,
@@ -9,18 +17,47 @@ import {
 } from "../../constants/colors";
 
 type Props = {|
+  label: string,
   placeholder: string,
   onFocus: () => void,
   onChangeText: string => void,
   returnKeyLabel?: string,
   returnKeyType?: "done" | "go" | "next" | "search" | "send",
   onSubmitEditing?: () => void,
+  style?: ViewStyleProp,
   value: ?string
 |};
 
 type State = {|
   focused: boolean
 |};
+
+const getAccessibiltyLabel = (
+  label: string,
+  value: ?string,
+  focused: boolean
+) => {
+  const labelParts = [];
+  if (focused) {
+    labelParts.push("editing");
+  }
+
+  if (value) {
+    labelParts.push(value);
+  }
+
+  labelParts.push("edit box");
+  labelParts.push(label);
+
+  const descriptionParts = [];
+  if (!focused) {
+    descriptionParts.push("double tap to enter text");
+  } else {
+    descriptionParts.push("double tap and hold to long press");
+  }
+
+  return [labelParts.join(", "), descriptionParts.join(", ")].join("; ");
+};
 
 class DonateScreen extends React.PureComponent<Props, State> {
   state = { focused: false };
@@ -35,25 +72,52 @@ class DonateScreen extends React.PureComponent<Props, State> {
   };
 
   render() {
-    const { placeholder, onFocus, ...otherProps } = this.props;
+    const { label, placeholder, onFocus, style, ...otherProps } = this.props;
     const { focused } = this.state;
+
+    // Note on accessibility:
+    // React Native does not support to mark up labels for input fields
+    // properly. See for example: https://github.com/facebook/react-native/issues/14989
+
     return (
-      <TextInput
-        keyboardType="numeric"
-        onBlur={this.onBlur}
-        onFocus={this.onFocus}
-        placeholder={focused ? placeholder : null}
-        placeholderTextColor={mediumGreyColor}
-        style={[styles.base, focused && styles.focused]}
-        underlineColorAndroid={transparent}
-        {...otherProps}
-      />
+      <View
+        accessible
+        accessibilityLabel={getAccessibiltyLabel(
+          label,
+          otherProps.value,
+          focused
+        )}
+        style={style}
+      >
+        <Text
+          type="h4"
+          color="lightNavyBlueColor"
+          importantForAccessibility="no"
+          style={styles.label}
+        >
+          {label}
+        </Text>
+        <TextInput
+          keyboardType="numeric"
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+          placeholder={focused ? placeholder : null}
+          placeholderTextColor={mediumGreyColor}
+          style={[styles.input, focused && styles.inputFocused]}
+          underlineColorAndroid={transparent}
+          importantForAccessibility="no"
+          {...otherProps}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  base: {
+  label: {
+    marginBottom: 8
+  },
+  input: {
     borderColor: mediumGreyColor,
     borderWidth: 1,
     borderRadius: 4,
@@ -70,7 +134,7 @@ const styles = StyleSheet.create({
       }
     })
   },
-  focused: {
+  inputFocused: {
     borderColor: eucalyptusGreenColor
   }
 });
