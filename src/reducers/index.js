4850c03519a5dd4449da3942a56cf5bc7d93d217
RGBboy <l-_-l@rgbboy.com>
2018-05-18 18:52:09 +0100

    add globalFilters to root reducer;

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,25 @@
 // @flow
 import { combineReducers } from "redux";
+import { DateTime } from "luxon";
 import events from "./events";
 import type { State as EventsState } from "./events";
 import eventFilters from "./event-filters";
 import type { State as EventFiltersState } from "../data/event-filters";
 import savedEvents from "./saved-events";
 import type { SavedEvents as SavedEventsState } from "../data/event";
+import globalFilters from "./global-filters";
+import type { State as GlobalFiltersState } from "./global-filters";
 
 export type State = {
   events: EventsState,
   eventFilters: EventFiltersState,
+  globalFilters: GlobalFiltersState,
   savedEvents: SavedEventsState
 };
 
 export default combineReducers({
   events,
   eventFilters,
+  globalFilters: globalFilters(DateTime.local),
   savedEvents
 });
