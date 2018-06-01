faff02616183b26a802643825be1140ed2d8549d
Jan Kuehle <jan.kuehle@red-badger.com>
2018-06-01 16:48:19 +0100

    Remove test asset source cache

--- src/data/get-asset-source.js
+++ src/data/get-asset-source.js
@@ -10,19 +10,13 @@ export type ImageSource = {
   height: number
 };
 
-const cache: { [string]: ImageSource } = {};
-
 export default (getAssetById: string => Asset) => (
   fieldRef: FieldRef
 ): ImageSource => {
-  if (!cache[fieldRef.sys.id]) {
-    const asset = getAssetById(fieldRef.sys.id);
-    cache[fieldRef.sys.id] = {
-      uri: `https:${asset.fields.file[locale].url}`,
-      width: asset.fields.file[locale].details.image.width,
-      height: asset.fields.file[locale].details.image.height
-    };
-  }
-
-  return cache[fieldRef.sys.id];
+  const asset = getAssetById(fieldRef.sys.id);
+  return {
+    uri: `https:${asset.fields.file[locale].url}`,
+    width: asset.fields.file[locale].details.image.width,
+    height: asset.fields.file[locale].details.image.height
+  };
 };
