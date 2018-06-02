8e6766c1d17975711e410a86ab4f68ef8de75821
Jan Kuehle <jan.kuehle@red-badger.com>
2018-06-02 17:52:06 +0100

    Add comment to flow type

--- src/lib/navigation.js
+++ src/lib/navigation.js
@@ -1,4 +1,6 @@
 // @flow
+/* eslint-disable import/prefer-default-export */
+
 import type { ComponentType } from "react";
 import { withNavigationFocus as rnWithNavigationFocus } from "react-navigation";
 import type { NavigationScreenProp, NavigationState } from "react-navigation";
@@ -8,9 +10,12 @@ export type NavigationProps = {
   isFocused: boolean
 };
 
-/* eslint-disable import/prefer-default-export */
-export function withNavigationFocus<T>(
-  Component: ComponentType<T & NavigationProps>
-): ComponentType<$Diff<T, NavigationProps>> {
-  return rnWithNavigationFocus(Component);
-}
+// React Navigation has types for this function, but they don't work properly
+// https://github.com/react-navigation/react-navigation/blob/05cbd85d5ce2f9775a2cc7c76a206c7e6d224508/flow/react-navigation.js#L1120
+
+// Do you get a flow error?
+// Your connector props most likely don't match the component props.
+export const withNavigationFocus = <Props>(
+  Component: ComponentType<Props & NavigationProps>
+): ComponentType<$Diff<Props, NavigationProps>> => // HELLO: If flow error, read comment above
+  rnWithNavigationFocus(Component);
