04c9284a29f70097a42c13c7c09e216b7c052d4b
Abigail Coe <abigailcoe@gmail.com>
2018-06-06 11:01:14 +0100

    flag expression

--- scripts/google-maps-ios.sh
+++ scripts/google-maps-ios.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i '' '\/Google.*\\.[h|m]/d' ./node_modules/react-native-maps/lib/ios/AirMaps.xcodeproj/project.pbxproj
+sed -i '' -e '/Google.*\.[h|m]/d' ./node_modules/react-native-maps/lib/ios/AirMaps.xcodeproj/project.pbxproj
