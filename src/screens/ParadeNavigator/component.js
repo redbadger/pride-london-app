79223af90a7468838792672bceb7e8535d66007f
Abigail Coe <abigailcoe@gmail.com>
2018-06-07 17:33:43 +0100

    use top tab bar

--- src/screens/ParadeNavigator/component.js
+++ src/screens/ParadeNavigator/component.js
@@ -1,47 +1,41 @@
 // @flow
-import React from "react";
-import { View, StyleSheet } from "react-native";
 import { createMaterialTopTabNavigator } from "react-navigation";
 
 import { PARADE_INFORMATION, PARADE_MAP } from "../../constants/routes";
 import ParadeInformationScreen from "../ParadeInformationScreen";
 import ParadeMapScreen from "../ParadeMapScreen";
-import Text from "../../components/Text";
+import TopTabBar from "../../components/TopTabBar";
 import { lightTealColor, darkBlueGreyColor } from "../../constants/colors";
 import text from "../../constants/text";
+
+export const getTabTestId = (routeName: string) => {
+  switch (routeName) {
+    case PARADE_INFORMATION:
+      return "parade-information-button";
+    case PARADE_MAP:
+      return "parade-map-button";
+    default:
+      return "";
+  }
+};
 
 const ParadeTabNav = createMaterialTopTabNavigator(
   {
     [PARADE_MAP]: {
       screen: ParadeMapScreen,
       navigationOptions: {
-        tabBarLabel: () => (
-          <View style={styles.view}>
-            <Text testID="parade-map-button" type="xSmall" style={styles.text}>
-              {text.tabParadeMap}
-            </Text>
-          </View>
-        )
+        tabBarLabel: text.tabParadeMap
       }
     },
     [PARADE_INFORMATION]: {
       screen: ParadeInformationScreen,
       navigationOptions: {
-        tabBarLabel: () => (
-          <View style={styles.view}>
-            <Text
-              testID="parade-information-button"
-              type="xSmall"
-              style={styles.text}
-            >
-              {text.tabParadeStages}
-            </Text>
-          </View>
-        )
+        tabBarLabel: text.tabParadeStages
       }
     }
   },
   {
+    tabBarComponent: TopTabBar,
     swipeEnabled: false,
     animationEnabled: false,
     initialRouteName: PARADE_MAP,
@@ -53,20 +47,10 @@ const ParadeTabNav = createMaterialTopTabNavigator(
       style: {
         backgroundColor: darkBlueGreyColor
       },
-      upperCaseLabel: false
+      upperCaseLabel: false,
+      getTabTestID: getTabTestId
     }
   }
 );
-
-const styles = StyleSheet.create({
-  view: {
-    height: 33,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  text: {
-    color: lightTealColor
-  }
-});
 
 export default ParadeTabNav;
