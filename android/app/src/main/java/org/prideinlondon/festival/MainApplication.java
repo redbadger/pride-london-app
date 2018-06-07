8830a17f2fa31ff72786b359eb6cebbff76fe1df
Abigail Coe <abigailcoe@gmail.com>
2018-06-07 11:31:51 +0100

    remove spaces

--- android/app/src/main/java/org/prideinlondon/festival/MainApplication.java
+++ android/app/src/main/java/org/prideinlondon/festival/MainApplication.java
@@ -33,8 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            BugsnagReactNative.getPackage(),	
-            new RNReactNativeHapticFeedbackPackage(),	
+            BugsnagReactNative.getPackage(),
+            new RNReactNativeHapticFeedbackPackage(),
             new LottiePackage(),
             new SplashScreenReactPackage(),
             new MapsPackage(),
