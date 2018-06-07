aa5bc44c314b709e15867319bfd3a1d280730c59
Abigail Coe <abigailcoe@gmail.com>
2018-06-07 13:38:39 +0100

    listen for tab focus

--- src/screens/ParadeMapScreen/component.js
+++ src/screens/ParadeMapScreen/component.js
@@ -1,45 +1,62 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 import MapView, { Polyline, Marker } from "react-native-maps";
+import type { NavigationEventSubscription } from "react-navigation";
 
 import paradeCoordinates from "../../constants/parade-coordinates";
 import { velvetColor } from "../../constants/colors";
 import Text, { scaleWithFont } from "../../components/Text";
+import { withNavigationFocus } from "../../lib/navigation";
+import type { NavigationProps } from "../../lib/navigation";
 
-const ParadeMap = () => (
-  <MapView
-    testID="parade-map-screen"
-    style={StyleSheet.absoluteFill}
-    region={{
-      latitude: 51.51004,
-      longitude: -0.134192,
-      latitudeDelta: 0.018,
-      longitudeDelta: 0.000000041
-    }}
-  >
-    <Polyline
-      coordinates={paradeCoordinates}
-      strokeWidth={5}
-      strokeColor={velvetColor}
-      lineJoin="bevel"
-    />
-    <Marker coordinate={{ longitude: -0.14223, latitude: 51.51616 }}>
-      <View style={styles.markerView}>
-        <Text type="xSmall" color="whiteColor">
-          A
-        </Text>
-      </View>
-    </Marker>
-    <Marker coordinate={{ longitude: -0.1265, latitude: 51.50499 }}>
-      <View style={styles.markerView}>
-        <Text type="xSmall" color="whiteColor">
-          B
-        </Text>
-      </View>
-    </Marker>
-  </MapView>
-);
+export class ParadeMap extends Component<NavigationProps> {
+  focus: NavigationEventSubscription;
+  constructor(props: NavigationProps) {
+    super();
+    this.focus = props.navigation.addListener("willFocus", () => null);
+  }
+
+  componentWillUnmount() {
+    this.focus.remove();
+  }
+
+  render() {
+    return (
+      <MapView
+        testID="parade-map-screen"
+        style={StyleSheet.absoluteFill}
+        region={{
+          latitude: 51.51004,
+          longitude: -0.134192,
+          latitudeDelta: 0.018,
+          longitudeDelta: 0.000000041
+        }}
+      >
+        <Polyline
+          coordinates={paradeCoordinates}
+          strokeWidth={5}
+          strokeColor={velvetColor}
+          lineJoin="bevel"
+        />
+        <Marker coordinate={{ longitude: -0.14223, latitude: 51.51616 }}>
+          <View style={styles.markerView}>
+            <Text type="xSmall" color="whiteColor">
+              A
+            </Text>
+          </View>
+        </Marker>
+        <Marker coordinate={{ longitude: -0.1265, latitude: 51.50499 }}>
+          <View style={styles.markerView}>
+            <Text type="xSmall" color="whiteColor">
+              B
+            </Text>
+          </View>
+        </Marker>
+      </MapView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   markerView: {
@@ -52,4 +69,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ParadeMap;
+export default withNavigationFocus(ParadeMap);
