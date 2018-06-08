938047c0863fc797e026f5e5bafd526136512a24
Nico Castro <nicollecastrog@gmail.com>
2018-06-08 13:15:24 +0100

    default/placeholder location card

--- src/screens/ParadeMapScreen/component.js
+++ src/screens/ParadeMapScreen/component.js
@@ -7,6 +7,7 @@ import type { NavigationEventSubscription } from "react-navigation";
 import paradeCoordinates from "../../constants/parade-coordinates";
 import { velvetColor } from "../../constants/colors";
 import Text, { scaleWithFont } from "../../components/Text";
+import LocationCard from "./LocationCard";
 import { withNavigationFocus } from "../../lib/navigation";
 import type { NavigationProps } from "../../lib/navigation";
 
@@ -24,42 +25,49 @@ export class ParadeMap extends Component<NavigationProps> {
 
   render() {
     return (
-      <MapView
-        testID="parade-map-screen"
-        style={StyleSheet.absoluteFill}
-        region={{
-          latitude: 51.51004,
-          longitude: -0.134192,
-          latitudeDelta: 0.018,
-          longitudeDelta: 0.000000041
-        }}
-      >
-        <Polyline
-          coordinates={paradeCoordinates}
-          strokeWidth={5}
-          strokeColor={velvetColor}
-          lineJoin="bevel"
-        />
-        <Marker coordinate={{ longitude: -0.14223, latitude: 51.51616 }}>
-          <View style={styles.markerView}>
-            <Text type="xSmall" color="whiteColor">
-              A
-            </Text>
-          </View>
-        </Marker>
-        <Marker coordinate={{ longitude: -0.1265, latitude: 51.50499 }}>
-          <View style={styles.markerView}>
-            <Text type="xSmall" color="whiteColor">
-              B
-            </Text>
-          </View>
-        </Marker>
-      </MapView>
+      <View style={styles.container} testID="parade-map-screen">
+        <MapView
+          style={StyleSheet.absoluteFill}
+          region={{
+            latitude: 51.51004,
+            longitude: -0.134192,
+            latitudeDelta: 0.018,
+            longitudeDelta: 0.000000041
+          }}
+        >
+          <Polyline
+            coordinates={paradeCoordinates}
+            strokeWidth={5}
+            strokeColor={velvetColor}
+            lineJoin="bevel"
+          />
+          <Marker coordinate={{ longitude: -0.14223, latitude: 51.51616 }}>
+            <View style={styles.markerView}>
+              <Text type="xSmall" color="whiteColor">
+                A
+              </Text>
+            </View>
+          </Marker>
+          <Marker coordinate={{ longitude: -0.1265, latitude: 51.50499 }}>
+            <View style={styles.markerView}>
+              <Text type="xSmall" color="whiteColor">
+                B
+              </Text>
+            </View>
+          </Marker>
+        </MapView>
+        <LocationCard />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flexDirection: "column",
+    justifyContent: "flex-end"
+  },
   markerView: {
     height: Math.max(15, scaleWithFont("xSmall", 18)),
     width: Math.max(15, scaleWithFont("xSmall", 18)),
