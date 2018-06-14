1f03df82c6baee3e96a7b6ca033fa444f7f0c01b
Abigail Coe <abigailcoe@gmail.com>
2018-06-14 11:11:47 +0100

    test prop

--- src/screens/ParadeMapScreen/component.test.js
+++ src/screens/ParadeMapScreen/component.test.js
@@ -4,6 +4,6 @@ import { shallow } from "enzyme";
 import ParadeMapScreen from "./component";
 
 it("renders correctly", () => {
-  const output = shallow(<ParadeMapScreen />);
+  const output = shallow(<ParadeMapScreen navigation={null} />);
   expect(output).toMatchSnapshot();
 });
