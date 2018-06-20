c1639c7e254443eeb3a95413a84f7652b7285187
RGBboy <l-_-l@rgbboy.com>
2018-06-20 09:23:04 +0100

    use initialRegion instead of region for map view;

--- src/screens/ParadeMapScreen/component.test.js
+++ src/screens/ParadeMapScreen/component.test.js
@@ -4,6 +4,11 @@ import { shallow } from "enzyme";
 import ParadeMapScreen from "./component";
 
 it("renders correctly", () => {
+  const output = shallow(<ParadeMapScreen isFocused />);
+  expect(output).toMatchSnapshot();
+});
+
+it("does not render map when not focused", () => {
   const output = shallow(<ParadeMapScreen />);
   expect(output).toMatchSnapshot();
 });
