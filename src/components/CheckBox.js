7508ae32edfeb5dde581ab5a4879d60211b4dba6
Jan Kuehle <jan.kuehle@red-badger.com>
2018-06-23 11:29:00 +0100

    Test for VoiceOver

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,10 +1,9 @@
 // @flow
 import React from "react";
-import { Image, StyleSheet } from "react-native";
+import { findNodeHandle, Image, StyleSheet, UIManager } from "react-native";
 import type { ViewStyleProp } from "react-native/Libraries/StyleSheet/StyleSheet";
 import Text from "./Text";
 import Touchable from "./Touchable";
-import { checkboxAccessibilityLabel } from "../data/accessibility";
 
 import checkboxUrl from "../../assets/images/checkBox.png";
 import checkBoxCheckedUrl from "../../assets/images/checkBoxChecked.png";
@@ -16,22 +15,39 @@ type Props = {
   style?: ViewStyleProp
 };
 
-const CheckBox = ({ checked, label, onChange, style }: Props) => (
-  <Touchable
-    accessibilityComponentType="none"
-    accessibilityTraits={checked ? ["selected"] : ["none"]}
-    accessibilityLabel={checkboxAccessibilityLabel(label, checked)}
-    onPress={onChange}
-    style={[styles.container, style]}
-  >
-    <Text style={styles.text}>{label}</Text>
-    <Image source={checked ? checkBoxCheckedUrl : checkboxUrl} />
-  </Touchable>
-);
+class CheckBox extends React.PureComponent<Props> {
+  static defaultProps = {
+    style: {}
+  };
 
-CheckBox.defaultProps = {
-  style: {}
-};
+  componentDidUpdate(prevProps: Props) {
+    // Notify user, that the radio button changed to checked state.
+    if (this.props.checked && !prevProps.checked) {
+      UIManager.sendAccessibilityEvent(
+        findNodeHandle(this),
+        UIManager.AccessibilityEventTypes.typeViewClicked
+      );
+    }
+  }
+
+  render() {
+    const { checked, label, onChange, style } = this.props;
+
+    return (
+      <Touchable
+        accessibilityComponentType={
+          checked ? "radiobutton_checked" : "radiobutton_unchecked"
+        }
+        accessibilityTraits={checked ? ["selected", "button"] : ["button"]}
+        onPress={onChange}
+        style={[styles.container, style]}
+      >
+        <Text style={styles.text}>{label}</Text>
+        <Image source={checked ? checkBoxCheckedUrl : checkboxUrl} />
+      </Touchable>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
