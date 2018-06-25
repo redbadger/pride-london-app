eff64d4d34544a6d3cb451aac589778287f7d098
RGBboy <l-_-l@rgbboy.com>
2018-06-25 16:19:14 +0100

    Use real data for parade groups screen

--- src/screens/ParadeGroupsScreen/index.js
+++ src/screens/ParadeGroupsScreen/index.js
@@ -6,8 +6,6 @@ import type { State } from "../../reducers";
 import withIsFocused from "../../components/WithIsFocused";
 import Component from "./component";
 
-import { generateParadeGroup, sampleArrayOf } from "../../data/__test-data";
-
 type OwnProps = {
   isFocused: boolean
 };
@@ -18,18 +16,13 @@ type Props = {
 
 let cache: Props;
 
-// @TODO: REMOVE THIS BEFORE MERGING TO MASTER
-const paradeGroups = sampleArrayOf(generateParadeGroup)(200);
-
-paradeGroups[0].fields.name = "Above the Stag Theatre";
-
 // Note we must add a return type here for react-redux connect to work
 // with flow correctly. If not provided is silently fails if types do
 // not line up. See https://github.com/facebook/flow/issues/5343
 const mapStateToProps = (state: State, { isFocused }: OwnProps): Props => {
   if (!cache || isFocused) {
     cache = {
-      paradeGroups
+      paradeGroups: state.data.paradeGroups
     };
   }
   return cache;
