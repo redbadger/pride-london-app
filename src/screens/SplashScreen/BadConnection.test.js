9607e2629b69595e512229864cd3c67a69bcf4f2
Jon Yardley <jon.yardley@red-badger.com>
2018-06-30 15:40:55 +0100

    Add tests for BadConnection component

--- src/screens/SplashScreen/BadConnection.test.js
+++ src/screens/SplashScreen/BadConnection.test.js
@@ -1,7 +1,9 @@
 // @flow
 import React from "react";
+import { ActivityIndicator } from "react-native";
 import { shallow } from "enzyme";
 import BadConnection from "./BadConnection";
+import Button from "../../components/ButtonPrimary";
 
 it("renders correctly when showing", () => {
   const output = shallow(
@@ -25,4 +27,31 @@ it("renders correctly when with no data and loading", () => {
   );
 
   expect(output).toMatchSnapshot();
+});
+
+describe("Retrying", () => {
+  it("Calls getData when retry button is pressed", () => {
+    const mock = jest.fn();
+    const output = shallow(
+      <BadConnection getData={mock} noDataReceived loading={false} />
+    );
+
+    const button = output.find(Button);
+    button.props().onPress();
+    expect(mock).toHaveBeenCalled();
+  });
+
+  it("Sets retrying state and renders spinner when retry button is pressed", () => {
+    const mock = jest.fn();
+    const output = shallow(
+      <BadConnection getData={mock} noDataReceived loading={false} />
+    );
+
+    const button = output.find(Button);
+    button.props().onPress();
+    output.update();
+    expect(output.state().retrying).toEqual(true);
+    expect(output.find(ActivityIndicator).exists()).toEqual(true);
+    expect(output.find(Button).exists()).toEqual(false);
+  });
 });
