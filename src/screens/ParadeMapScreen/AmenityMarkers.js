5bb08a3d57095960b52571de17134a6713395d45
RGBboy <l-_-l@rgbboy.com>
2018-07-09 18:00:09 +0100

    add correct z-index to amenity markers;

--- src/screens/ParadeMapScreen/AmenityMarkers.js
+++ src/screens/ParadeMapScreen/AmenityMarkers.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { Fragment } from "react";
+import { StyleSheet } from "react-native";
 import { Marker } from "react-native-maps";
 import type { Amenity } from "../../data/amenity";
 import type { Coordinates } from "../../constants/parade-coordinates";
@@ -45,6 +46,7 @@ const AmenityMarkers = ({
           stopPropagation
           onPress={() => handleMarkerPress(amenity.id)}
           key={amenity.id}
+          style={activeMarker === amenity.id ? styles.active : styles.inactive}
           image={
             activeMarker === amenity.id
               ? activeIconMap[amenity.fields.type]
@@ -56,5 +58,16 @@ const AmenityMarkers = ({
     </Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  inactive: {
+    zIndex: 1
+  },
+  active: {
+    // React Native Maps adds a constant to the currently open callout, but if the stage marker is active we want to move it above this
+    // https://github.com/react-community/react-native-maps/blob/080678b24f886c3b8104206f2f80452ee723243a/lib/ios/AirMaps/AIRMapMarker.m#L316
+    zIndex: 1001
+  }
+});
 
 export default AmenityMarkers;
